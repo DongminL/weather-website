ea0ab10e4ffb4305254248b23bc8d3f2e4688b60
DongminL <milkskfk12@gmail.com>
2025-05-31 22:44:07 +0900

    style: 메인 페이지 최소 화면 구성

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,36 @@
 import { useEffect, useState } from 'react'
+import { useRouter } from "next/router";
 
-export default function Home() {
+export default function Main() {
+    const router = useRouter();
+    const cities = ["Seoul", "Tokyo", "Paris", "London"];
 
     return (
         <main>
-            <h1>Weather App</h1>
+            <div>
+                <header>
+                    <h1>
+                        Welcome to <span>Weather App!</span>
+                    </h1>
+                    <p>
+                        Choose a city from the list below to check the weather.
+                    </p>
+                </header>
+
+                <nav>
+                    {cities.map((city) => (
+                        <button
+                            onClick={() => router.push(`/${city}`)}
+                        >
+                            {city}
+                        </button>
+                    ))}
+                </nav>
+
+                <figure>
+                    <img src="/earth.svg"/>
+                </figure>
+            </div>
         </main>
-    )
+    );
 }
