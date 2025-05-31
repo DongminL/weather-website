3e8be705ec988eff21da0014beefbf169f746013
DongminL <milkskfk12@gmail.com>
2025-06-01 00:03:02 +0900

    style: 현재 날씨 부분 최소 화면 구성

---
+++ src/features/weather/components/current.js
@@ -0,0 +1,51 @@
+import { useEffect, useState } from "react";
+import { WeatherProvider } from "../services/weatherProvider";
+import { Cities } from "../dtos/city";
+
+export default function CurrentCityWeather({ cityName }) {
+    const [weatherData, setWeatherData] = useState(null);
+
+    useEffect(() => {
+        const fetchWeather = async () => {
+            const city = Cities.fromValue(cityName);
+
+            try {
+                const weather = await new WeatherProvider().getCurrentWeatherByCity(city);
+                setWeatherData(weather);
+            } catch (err) {
+                console.error("날씨 정보를 가져오는 중 오류 발생:", err);
+            }
+        };
+
+        fetchWeather();
+    }, [cityName]);
+
+    return (
+        <section>
+            {weatherData ? (
+                <div>
+                <img src={`https://openweathermap.org/img/wn/${weatherData.info.icon}@2x.png`} />
+                <div>
+                    <p>May 23. 03:00am</p>
+                    <h2>
+                        {cityName}, {weatherData.location.country}
+                    </h2>
+                    <p>
+                        (인구수: 10343912)
+                    </p>
+                </div>
+                <div>
+                    <p>
+                        {weatherData.weather.temp}°C
+                    </p>
+                    <p>
+                        Feels like {weatherData.weather.feelsLike}°C {weatherData.info.description} 풍속 {weatherData.wind.speed}m/s 습도{weatherData.weather.humidity}%
+                    </p>
+                </div>
+                </div>
+            ) : (
+                <p>현재 날씨 데이터를 불러오는 중...</p>
+            )}
+        </section>
+    );
+}
