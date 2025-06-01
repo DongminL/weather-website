06f16e5c8dea5fe092350256a17b79fb55d340cf
DongminL <milkskfk12@gmail.com>
2025-06-02 04:52:31 +0900

    fix: apollo server 중복 실행 오류 해결

--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -2,18 +2,25 @@ import { ApolloServer } from "apollo-server-micro";
 import { typeDefs } from "@/features/weather/graphql/schema";
 import { resolvers } from "@/features/weather/graphql/resolver";
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-});
-
 export const config = {
     api: {
         bodyParser: false,
-    }
-}
+    },
+};
+
+let apolloServer = null;
+let apolloHandler = null;
 
 export default async function handler(req, res) {
-    await server.start();
-    return server.createHandler({ path: "/api/weather" })(req, res);
+    if (!apolloServer) {
+        apolloServer = new ApolloServer({
+            typeDefs,
+            resolvers,
+        });
+
+        await apolloServer.start();
+        apolloHandler = apolloServer.createHandler({ path: "/api/weather" });
+    }
+
+    return apolloHandler(req, res);
 }
