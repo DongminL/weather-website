2f044d0ef0e39e0fe85c1c20959706d8ff915d9f
DongminL <milkskfk12@gmail.com>
2025-06-02 21:03:41 +0900

    feat: timezone 필드 추가

--- src/features/weather/dtos/city.js
+++ src/features/weather/dtos/city.js
@@ -1,28 +1,21 @@
-class Coordinate {
-    constructor(lat, lon) {
-        this._lat = lat;
-        this._lon = lon;
-    }
-
-    get lat() {
-        return this._lat;
-    }
-
-    get lon() {
-        return this._lon;
+class City {
+    constructor(lat, lon, timezone) {
+        this.lat = lat;
+        this.lon = lon;
+        this.timezone = timezone;
     }
 }
 
 export const Cities = Object.freeze({
-    SEOUL: Object.freeze(new Coordinate(37.5666791, 126.9782914)),
-    TOKYO: Object.freeze(new Coordinate(35.6828387, 139.7594549)),
-    PARIS: Object.freeze(new Coordinate(48.8588897, 2.3200410217200766)),
-    LONDON: Object.freeze(new Coordinate(51.5073219, -0.1276474)),
+    SEOUL: Object.freeze(new City(37.5666791, 126.9782914, "Asia/Seoul")),
+    TOKYO: Object.freeze(new City(35.6828387, 139.7594549, "Asia/Tokyo")),
+    PARIS: Object.freeze(new City(48.8588897, 2.3200410217200766, "Europe/Paris")),
+    LONDON: Object.freeze(new City(51.5073219, -0.1276474, "Europe/London")),
 
     fromValue(value) {
         const key = String(value).toUpperCase();
 
-        if (!this[key] instanceof Coordinate) {
+        if (!this[key] instanceof City) {
             throw new Error("존재하지 않는 도시입니다.");
         }
         return this[key];
