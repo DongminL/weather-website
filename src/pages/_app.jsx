027ab07496974aff25f70a30b7a5a2952e3882a9
DongminL <milkskfk12@gmail.com>
2025-06-03 04:29:31 +0900

    style: 반응형 웹 설정

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -5,7 +5,9 @@ import client from '@/graphql/apolloClient';
 export default function MyApp({ Component, pageProps }) {
     return (
         <ApolloProvider client={client}>
-            <Component {...pageProps} />
+            <div className="appLayout">
+                <Component {...pageProps} />
+            </div>
         </ApolloProvider>
     );
 }
